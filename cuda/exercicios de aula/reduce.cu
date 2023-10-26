926d96a672365f24a7bf28d8f60c0190fe46e50d
jcamachado <jcamachado@id.uff.br>
2023-10-26 18:51:25 -0300

    feat: add reduce com extern e shared memory

---
+++ cuda/exercicios de aula/reduce.cu
@@ -0,0 +1,71 @@
+#include <math.h>
+#include <stdio.h>
+
+#define THREADS 8 // 1000 threads per block
+#define ARRAYSIZE 9
+
+__global__ void reduceSum(int *data_in, int *data_out){
+    extern __shared__ int s_data[];
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    int tid = threadIdx.x;
+    s_data[tid] = data_in[tid];
+    __syncthreads();
+
+    for (int stride = blockDim.x/2; stride > 0; stride >>=1) {
+        if (tid < stride){
+            s_data[index] += s_data[index+stride];
+        }
+    __syncthreads();
+    }
+    if(tid == 0){
+        data_out[tid] = s_data[0];
+    }
+
+}
+
+int main(){
+    int *a, *d_a, lastElement, *d_out;
+    int new_array_size = ARRAYSIZE;
+
+    a = (int*) malloc(sizeof(int) * ARRAYSIZE);    
+
+    for(int i = 0; i < ARRAYSIZE; i++){
+        a[i] = i;
+    }
+    printf("\nEntrada\n");
+    for(int i = 0; i < ARRAYSIZE; i++){
+        printf("%d ", a[i]);
+    }
+    if (ARRAYSIZE % 2 != 0){
+        new_array_size--;
+        lastElement = a[new_array_size];
+        a = (int*) realloc(a, sizeof(int) * new_array_size);  
+    }
+
+    printf("\nEntrada normalizada\n");
+    
+    for(int i = 0; i < new_array_size; i++){
+        printf("%d ", a[i]);
+    }       
+
+    cudaMalloc((void**) &d_a, sizeof(int) * new_array_size);
+    cudaMemcpy(d_a, a, sizeof(int) * new_array_size, cudaMemcpyHostToDevice);
+    
+    cudaMalloc((void**) &d_out, sizeof(int));
+    cudaMemcpy(d_out, 0, sizeof(int), cudaMemcpyHostToDevice);
+
+    reduceSum<<<1,THREADS, new_array_size*sizeof(int)>>>(d_a, d_out);
+
+    cudaMemcpy(a, d_out, sizeof(int), cudaMemcpyDeviceToHost);
+
+    printf("\nSaida: \n");
+    a[0] = a[0] + lastElement;
+    printf("soma = %d ", a[0]);
+    
+    free(a);
+    cudaFree(d_a);
+    cudaFree(d_out);
+    printf("\nFIM");
+
+    return 0;
+}
