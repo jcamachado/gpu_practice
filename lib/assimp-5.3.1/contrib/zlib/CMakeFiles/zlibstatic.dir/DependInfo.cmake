afb9cd263f2217e90f151b833d140088ee332d7a
jcamachado <jcamachado@id.uff.br>
2023-11-10 19:41:43 -0300

    chore: add assimp lib

---
+++ lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/adler32.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/adler32.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/adler32.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/compress.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/compress.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/compress.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/crc32.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/crc32.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/crc32.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/deflate.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/deflate.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/deflate.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/gzclose.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/gzclose.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/gzclose.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/gzlib.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/gzlib.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/gzlib.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/gzread.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/gzread.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/gzread.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/gzwrite.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/gzwrite.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/gzwrite.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/infback.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/infback.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/infback.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/inffast.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/inffast.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/inffast.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/inflate.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/inflate.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/inflate.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/inftrees.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/inftrees.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/inftrees.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/trees.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/trees.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/trees.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/uncompr.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/uncompr.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/uncompr.c.o.d"
+  "/home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib/zutil.c" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/zutil.c.o" "gcc" "lib/assimp-5.3.1/contrib/zlib/CMakeFiles/zlibstatic.dir/zutil.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lib/assimp-5.3.1/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib
+# Build directory: /home/jaxe/Repositories/gpu_study/gpu_practice/lib/assimp-5.3.1/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
