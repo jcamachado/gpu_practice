79c957e570225ec17912e6988a2fb04b86922345
jaxe <jcamachado@id.uff.br>
2024-11-03 19:28:19 -0300

    feat: make clear color black

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -125,7 +125,8 @@ namespace ud {
         renderPassInfo.renderArea.extent = udSwapChain->getSwapChainExtent();
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = { 0.1f, 0.1f, 0.1f, 1.0f };
+        // clearValues[0].color = { 0.1f, 0.1f, 0.1f, 1.0f };
+        clearValues[0].color = { 0.0f, 0.0f, 0.0f, 1.0f }; // Pitch black color
         clearValues[1].depthStencil = { 1.0f, 0 };
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
         renderPassInfo.pClearValues = clearValues.data();
